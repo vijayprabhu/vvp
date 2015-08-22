0fd00c886d22f3afcac0aebb03aa6cd8f55eb9ab
vijayprabhu <vijayprabhuv@gmail.com>
2015-08-22 18:08:03 -0400

    Initial Test Commit

---
+++ my-first-spring-boot-web/src/main/java/person/me/MyFirstSpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package person.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstSpringBootWebApplication.class, args);
+    }
+}
