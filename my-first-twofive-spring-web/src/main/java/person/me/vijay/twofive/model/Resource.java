7aa2a809a7c16d5001de1cbc182fc153a423695b
vijayprabhu <vijayprabhuv@gmail.com>
2015-08-23 12:42:48 -0400

    Added more...

---
+++ my-first-twofive-spring-web/src/main/java/person/me/vijay/twofive/model/Resource.java
@@ -0,0 +1,26 @@
+package person.me.vijay.twofive.model;
+
+public class Resource {
+	private String ip;
+	private String hostname;
+	private String jvmName;
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+	public String getJvmName() {
+		return jvmName;
+	}
+	public void setJvmName(String jvmName) {
+		this.jvmName = jvmName;
+	}
+	
+}
--- my-first-twofive-spring-web/src/main/java/person/me/vijay/twofive/rc/MyResourceController.java
+++ my-first-twofive-spring-web/src/main/java/person/me/vijay/twofive/rc/MyResourceController.java
@@ -1,15 +1,36 @@
 package person.me.vijay.twofive.rc;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import person.me.vijay.twofive.model.Resource;
+
 @RestController
-@RequestMapping("myresources")
+@RequestMapping("keepalive")
 public class MyResourceController {
 	
-	@RequestMapping("/get")
-	public String getResource() {
-		return "Hello Resource";
+	@RequestMapping("/status")
+	public String getStatus() {
+		return "I'm up and Running";
 	}
 	
+	
+	@RequestMapping("/config")
+	
+	public Resource getConfig() {
+		
+		Resource resource = new Resource();
+		try {
+			resource.setHostname(Inet4Address.getLocalHost().getHostName());
+			resource.setIp(Inet4Address.getLocalHost().getHostAddress());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return resource;
+	}
 }
